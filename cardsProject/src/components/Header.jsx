403f2a57712482266e6d47cdb6461a34e57b425b
Vitalii Golovko <vitaliy1337g@gmail.com>
2023-11-05 14:57:12 +0100

    Add home logo, start design poker page.

--- cardsProject/src/components/Header.jsx
+++ cardsProject/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import '../styles/Header.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 export const Header = () => {
   return (
     <header className="header"> 
+      <Link to='/'><FontAwesomeIcon icon={faHouse} className="home-icon"/></Link> 
       <Link to='/durak' className="nav-link">Durak</Link> 
       <Link to='/poker' className="nav-link">Poker</Link> 
       <Link to='/patience' className="nav-link">Patience</Link> 
--- cardsProject/src/components/Poker.jsx
+++ cardsProject/src/components/Poker.jsx
@@ -1,3 +1,19 @@
+import '../styles/Poker.css';
+
 export const Poker = () => {
-  return <h1>Poker</h1>
-}
+  return (
+    <div className="poker-container">
+      <div className="image-container">
+        <img src="../public/poker.png" className='poker-img' alt="Poker Image" />
+        <div className="text-overlay">
+          <h1>LEARN TO PLAY</h1>
+          <h3>Basics of Poker</h3>
+          <h6>
+            Today, Poker is truly an international game, enjoyed in virtually every
+            country where card games are played.
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
