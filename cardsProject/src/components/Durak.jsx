7711eac0dbe10c7b945cec617b316bbf204959e4
Vitalii Golovko <vitaliy1337g@gmail.com>
2023-11-07 08:59:55 +0100

    start designing header pages

--- cardsProject/src/components/Durak.jsx
+++ cardsProject/src/components/Durak.jsx
@@ -1,3 +1,19 @@
+import '../styles/Durak.scss';
+
 export const Durak = () => {
-  return <h1>Durak</h1>
-}
+  return (
+    <div className="durak-container">
+      <div className="image-container">
+        <img src="../public/durak.png" className='poker-img' alt="Poker Image" />
+        <div className="text-overlay">
+          <h1>LEARN TO PLAY</h1>
+          <h3>Basics of Durak</h3>
+          <h6>
+            Today, Durak is truly an international game, enjoyed in virtually every
+            country where card games are played.
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
--- cardsProject/src/components/Patience.jsx
+++ cardsProject/src/components/Patience.jsx
@@ -1,3 +1,19 @@
+import '../styles/Patience.scss';
+
 export const Patience = () => {
-  return <h1>Patience</h1>
-}
+  return (
+    <div className="poker-container">
+      <div className="image-container">
+        <img src="../public/poker.png" className='poker-img' alt="Poker Image" />
+        <div className="text-overlay">
+          <h1>LEARN TO PLAY</h1>
+          <h3>Basics of Patience</h3>
+          <h6>
+            Today, Patience is truly an international game, enjoyed in virtually every
+            country where card games are played.
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
